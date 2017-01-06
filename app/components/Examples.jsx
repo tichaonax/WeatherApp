05b1f87e9ae7386fbd996b4023762af46fe39635
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-06 11:45:35 -0600

    Styled Examples and Main page with Foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,24 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 class Examples extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
-                <h2>Welcome to Examples page</h2>
+                <h1 className="text-center">Examples</h1>
+                <p> Try out the following locations</p>
+                <ol>
+                    <li>
+                        <Link to="/?location=Gutu">Gutu, Zimbabwe</Link>
+                    </li>
+                    <li>
+                        <Link to="/?location=Harare">Harare, Zimbabwe</Link>
+                    </li>
+                </ol>
             </div>
         );
     }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,14 +2,21 @@ var React = require('react');
 var Nav = require('Nav');
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
                 <div>
                     <Nav/>
+                    <div className="row">
+                        <div className="columns medium-6 large-4 small-centered">
+                            {this.props.children}
+                        </div>
+                    </div>
                 </div>
-                <h2>Main Component</h2>
-                {this.props.children}
             </div>
         );
     }
