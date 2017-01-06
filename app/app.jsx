6f157a7fc8e4cc684f1e85ca3d64dd9fb6131e07
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-06 17:53:35 -0600

    added support for custom styling

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,10 @@ var Examples = require('Examples');
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
+// App css
+
+require('style!css!applicationStyles')
+
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
