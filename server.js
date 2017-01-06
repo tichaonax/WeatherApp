7636b59ddef2c56ae154de2964b3ea8b12692bb2
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-05 18:33:10 -0600

    refactored to support heroku hosting integrations

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 
-// Create our app
 var app = express();
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+app.use((request, response, next) => {
+
+    if (!request.secure) {
+        next();
+    } else {
+        response.redirect('http://' + request.hostname + request.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
-});
+app.listen(SERVER_PORT, ()=>  console.log('Express server is up on port ' + SERVER_PORT));
